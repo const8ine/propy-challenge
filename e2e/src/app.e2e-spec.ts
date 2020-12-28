6c2c43b3d872928cdb5745634348e3c1fbaac40c
Constantine <kon.rafikov@yandex.ru>
2020-12-28 18:43:42 +0300

    Changed project name remained from a boilerplate

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('faketweets app is running!');
+    expect(await page.getTitleText()).toEqual('propychallenge app is running!');
   });
 
   afterEach(async () => {
