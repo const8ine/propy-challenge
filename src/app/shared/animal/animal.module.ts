92ff6d29753d0b2b18660c6f8c2d44886f14473a
Constantine <kon.rafikov@yandex.ru>
2020-12-28 22:25:44 +0300

    Name component. Click leads to Settings page. Styles.

--- src/app/shared/animal/animal.module.ts
+++ src/app/shared/animal/animal.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SoundComponent } from './components/sound/sound.component';
 import { NameComponent } from './components/name/name.component';
+import {RouterModule} from "@angular/router";
 import {IconModule} from "../icon/app-icon.module";
 
 @NgModule({
@@ -11,6 +12,7 @@ import {IconModule} from "../icon/app-icon.module";
   ],
   imports: [
     CommonModule,
+    RouterModule,
     IconModule
   ]
 })
